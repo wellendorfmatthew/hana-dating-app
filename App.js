dc1f4f8649b2def031b60a58ec270a63678518f7
wellendorfmatthew <wellendorfmatthew@gmail.com>
2023-12-18 14:41:17 -0800

    Added messages page

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import RelationshipInfo from "./Forms/RelationshipInfo";
 import InterestsInfo from "./Forms/InterestsInfo";
 import PhotosInfo from "./Forms/PhotosInfo";
 import Matching from "./Forms/Matching";
+import Messages from "./Forms/Messages";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,7 @@ export default function App() {
         <Stack.Screen name='InterestsInfo' component={InterestsInfo} />
         <Stack.Screen name='PhotosInfo' component={PhotosInfo} />
         <Stack.Screen name='Matching' component={Matching} />
+        <Stack.Screen name='Messages' component={Messages} />
       </Stack.Navigator>
     </NavigationContainer>
   )
